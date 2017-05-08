91ce3df8f3c2d4270ebe42f6d5307dca37d3a4cb
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-05-08 19:40:52 +1000

    fillArrayChegg working. Now need to add row columns and row totals

--- arraySolver/fillArrayChegg/main.c
+++ arraySolver/fillArrayChegg/main.c
@@ -3,74 +3,78 @@
 
 int getSize()
 {
-FILE * pointer;
-int count;
+    FILE * pointer;
+    int count;
 
-pointer = fopen("testTest.txt", "r");
+    pointer = fopen("../testTest.txt", "r");
 
-if(pointer == NULL)
-{
-printf("Open operation failed.");
-return 1;
+    if(pointer == NULL)
+    {
+        printf("Open operation failed.");
+        return 1;
+    }
+
+    fscanf(pointer, "%d", &count);
+    fclose(pointer);
+    return count;
 }
 
-fscanf(pointer, "%d", &count);
-fclose(pointer);
-return count;
-}
 void fillArray(int **array, int count)
 {
-FILE * pointer;
-int i, j, x;
+    FILE * pointer;
+    int i, j, x;
 
-pointer = fopen("testTest.txt", "r");
+    pointer = fopen("../testTest.txt", "r");
 
-// skipping size value
-fscanf(pointer, "%d", &x);
+    // skipping size value
+    fscanf(pointer, "%d", &x);
   
-for(i = 0; i < count; i++)
-{
-for(j = 0; j < count; j++)
-{
-fscanf(pointer, "%d",&array[i][j]);
-}
+    for(i = 0; i < count; i++)
+    {
+        for(j = 0; j < count; j++)
+        {
+            fscanf(pointer, "%d",&array[i][j]);
+        }
   
+    }
+
+    fclose(pointer);
 }
 
-fclose(pointer);
-}
 int main()
 {
-int x, y, size = 0, i;
+    int x, y, size = 0, i;
 
-size = getSize();
+    size = getSize();
 
-// creating memory for array
-int **myArray = (int **)malloc(size*sizeof(int *));
+    // creating memory for array
+    int **myArray = (int **)malloc(size*sizeof(int *));
 
-for(i=0; i<size; i++){
-myArray[i] = (int *)malloc(size*sizeof(int));
-}
+    for(i=0; i<size; i++)
+    {
+        myArray[i] = (int *)malloc(size*sizeof(int));
+    }
 
-fillArray(myArray, size);
+    fillArray(myArray, size);
   
-printf("size = %i\n",size);
-for(x = 0; x < size; x++)
-{
-for(y = 0; y < size; y++)
-{
-printf("%i ",myArray[x][y]);
-}
-printf("\n");
-}
+    printf("size = %i\n",size);
+    for(x = 0; x < size; x++)
+    {
+        for(y = 0; y < size; y++)
+        {
+            printf("%i ",myArray[x][y]);
+        }
+        printf("\n");
+    }
 
-// deleting memory
+    // deleting memory
 
-for(i=0; i<size; i++){
-free(myArray[i]);
-}
+    for(i=0; i<size; i++)
+    {
+        free(myArray[i]);
+    }
 
-free(myArray);
+    free(myArray);
 }
 
 
